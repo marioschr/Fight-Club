52af9ae1d2a79ea27183b349f1963934335d8020
Marios Chr <marios-christou@hotmail.com>
2021-03-29 01:05:01 +0300

    loading

--- Fight Club/Assets/Scripts/Manager.cs
+++ Fight Club/Assets/Scripts/Manager.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using Cinemachine;
 using Photon.Pun;
 using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
+using UnityEditor;
 using UnityEngine;
 
 namespace com.SikkimeStudios.FightClub
@@ -13,30 +15,41 @@ namespace com.SikkimeStudios.FightClub
     {
         public string player_prefab;
         public Transform[] spawn_points;
-        public CinemachineTargetGroup ctg;
+        public GameObject loading;
+        public CinemachineTargetGroup targetGroup;
+        private GameObject[] players;
+        private bool both = false;
         public void Start()
         { 
-            Spawn();
-            ctg = GetComponent<CinemachineTargetGroup>();
+            if(PhotonNetwork.IsMasterClient)
+            {
+                Debug.Log("master client");
+                Spawn(0);
+                //photonView.RPC("Spawn", RpcTarget.All, 0);
+            }
+            else
+            {
+                Debug.Log("non master client");
+                Spawn(1);
+                //photonView.RPC("Spawn", RpcTarget.All, 1);
+            }
         }
 
-        public void Spawn()
+        [PunRPC]
+        public void Spawn(int index)
         {
-            if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
-            {
-                GameObject player1 = PhotonNetwork.Instantiate(player_prefab, spawn_points[0].position, spawn_points[0].rotation);
-                ctg.AddMember(player1.transform, 1f, 0f);
-            } else if (PhotonNetwork.CurrentRoom.PlayerCount == 2)
-            {
-                GameObject player2 = PhotonNetwork.Instantiate(player_prefab, spawn_points[1].position, spawn_points[1].rotation);
-                ctg.AddMember(player2.transform, 1f, 0f);
-            }
-
+            GameObject player = PhotonNetwork.Instantiate(player_prefab, spawn_points[index].position, spawn_points[index].rotation);
+            targetGroup.AddMember(player.transform, 1f, 0.3f);
         }
 
         private void Update()
         {
-            
+            if (both) return;
+            if (GameObject.FindGameObjectsWithTag("Player").Length == 2)
+            {
+                both = true;
+                loading.SetActive(false);
+            }
         }
     }
 }
