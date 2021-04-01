36bad0f0999aa56c7fe0891cdb196dd26dee14b5
Marios Chr <marios-christou@hotmail.com>
2021-04-01 18:54:42 +0300

    Characters

--- Fight Club/Assets/Scripts/Manager.cs
+++ Fight Club/Assets/Scripts/Manager.cs
@@ -8,17 +8,39 @@ using Photon.Pun.UtilityScripts;
 using Photon.Realtime;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace com.SikkimeStudios.FightClub
 { 
     public class Manager : MonoBehaviourPunCallbacks
     {
+        public static Manager Instance = null;
+        public Text InfoText;
         public string player_prefab;
         public Transform[] spawn_points;
         public GameObject loading;
         public CinemachineTargetGroup targetGroup;
         private GameObject[] players;
         private bool both = false;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            CountdownTimer.OnCountdownTimerHasExpired += OnCountdownTimerIsExpired;
+        }
+
+        public override void OnDisable()
+        {
+            base.OnDisable();
+
+            CountdownTimer.OnCountdownTimerHasExpired -= OnCountdownTimerIsExpired;
+        }
         public void Start()
         { 
             if(PhotonNetwork.IsMasterClient)
@@ -46,6 +68,8 @@ namespace com.SikkimeStudios.FightClub
         {
             if (both) return;
             players = GameObject.FindGameObjectsWithTag("Player");
+            int startTimestamp;
+            bool startTimeIsSet = CountdownTimer.TryGetStartTime(out startTimestamp);
             if (players.Length == 2)
             {
                 both = true;
@@ -55,8 +79,13 @@ namespace com.SikkimeStudios.FightClub
                     player.GetPhotonView().RPC("SetHealthUI", RpcTarget.All);
                     player.GetPhotonView().RPC("SetStaminaUI", RpcTarget.All);
                 }
+                if (!startTimeIsSet)
+                {
+                    CountdownTimer.SetStartTime();
+                }
             }
         }
+        
         [PunRPC]
         void SetHealthUI()
         {
@@ -67,6 +96,69 @@ namespace com.SikkimeStudios.FightClub
         void SetStaminaUI()
         {
             GetComponent<Fighting>().SetStaminaUI();
+        }
+        
+        private IEnumerator EndOfGame(string winner, int score)
+        {
+            float timer = 5.0f;
+
+            while (timer > 0.0f)
+            {
+                InfoText.text = string.Format("Player {0} won with {1} points.\n\n\nReturning to login screen in {2} seconds.", winner, score, timer.ToString("n2"));
+
+                yield return new WaitForEndOfFrame();
+
+                timer -= Time.deltaTime;
+            }
+
+            PhotonNetwork.LeaveRoom();
+        }
+        
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
+
+        public override void OnLeftRoom()
+        {
+            PhotonNetwork.Disconnect();
+        }
+        
+        public override void OnPlayerLeftRoom(Player otherPlayer)
+        {
+            CheckEndOfGame();
+        }
+        
+        private void CheckEndOfGame()
+        {
+            if (PhotonNetwork.IsMasterClient)
+            {
+                StopAllCoroutines();
+            }
+
+            string winner = "";
+            int score = -1;
+
+            foreach (Player p in PhotonNetwork.PlayerList)
+            {
+                if (p.GetScore() > score)
+                {
+                    winner = p.NickName;
+                    score = p.GetScore();
+                }
+            }
+
+            StartCoroutine(EndOfGame(winner, score));
+        }
+
+        private void OnCountdownTimerIsExpired()
+        {
+            StartGame();
+        }
+
+        void StartGame()
+        {
+            Debug.Log("Start Game Method");
         }
     }
 }
